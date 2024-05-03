36024505cb297837864250c462a962cd5b7afb66
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-05-03 17:01:47 +1000

    Start script updated

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-import sys
+from argparse import ArgumentParser
 
 # TODO: Remove stubs, organize calling of necessary modules.
 launch_mode = {
@@ -7,17 +7,37 @@ launch_mode = {
 }
 
 
+def setup_mode_argument(parser: ArgumentParser) -> None:
+    parser.add_argument(
+        "-m",
+        "--mode",
+        default="CLI",
+        help="specifies in which mode the program should be launched (CLI/GUI). Default CLI.",
+        choices=("CLI", "GUI"),
+        metavar="",
+    )
+
+
 def main() -> None:
-    if len(sys.argv) < 2:
-        raise RuntimeError("Need to specify the application launch mode.")
+    arg_parser = ArgumentParser(
+        description="""
+            EVTX security logs viewer for Linux. \n
 
-    input_mode = sys.argv[1].upper()
-    mode = launch_mode.get(input_mode, "undefined")
+            Application designed specifically for Linux systems
+            for more convenient, fast and efficient viewing
+            Windows security logs in EVTX format.
 
-    if mode == "undefined":
-        raise RuntimeError(
-            f'The application cannot be launched in "{input_mode}" mode.'
-        )
+            The application supports both graphical and
+            console mode of operation, which allows everyone
+            Optimize your workflow the way you need it.
+        """
+    )
+
+    setup_mode_argument(parser=arg_parser)
+    launch_args = arg_parser.parse_args()
+
+    input_mode = launch_args.mode.upper()
+    mode = launch_mode[input_mode]
 
     mode()
 

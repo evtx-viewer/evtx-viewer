86f444f6d5ef08292ece990b2f2b1e89c48a3b2a
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-05-13 22:55:57 +1000

    Working function of validation inputp path

--- path/validator.py
+++ path/validator.py
@@ -1,28 +1,31 @@
 from pathlib import Path
-from typing import (
-    Optional,
-    List,
-)
+import sys
+from typing import Optional, List, Union
 
 
 # TODO: write me
-def validate_input(path: str) -> Optional[str | List[str]]:
-    path_object: Path = Path(path)
-    if path_object.is_dir():
-        files: List[str] = list(path_object.glob("*.evtx"))
-        if not files:
-            raise FileNotFoundError(
-                "Not a single EVTX security log was found in the directory."
-            )
+def validate_input(path: str) -> Optional[Union[str, List[str]]]:
+    try:
+        path_object: Path = Path(path)
+        if path_object.is_dir():
+            files: List[str] = list(path_object.glob("*.evtx"))
+            if not files:
+                raise FileNotFoundError(
+                    "Not a single EVTX security log was found in the directory."
+                )
 
-        return files
+            return [ptf.resolve()._str for ptf in files]
 
-    elif path_object.is_file():
-        extension: str = path_object.suffix
-        if extension != ".evtx":
-            raise ValueError("The file is not an EVTX security log.")
+        elif path_object.is_file():
+            extension: str = path_object.suffix
+            if extension != ".evtx":
+                raise ValueError("The file is not an EVTX security log.")
 
-        return path
+            return path_object.resolve()._str
 
-    else:
-        raise ValueError("Wrong path.")
+        else:
+            raise ValueError("This is not a file or a directory.")
+
+    except (ValueError, FileNotFoundError) as error:
+        sys.stdout.write(str(error) + "\n")
+        return None

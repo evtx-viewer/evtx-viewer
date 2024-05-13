0970ef859f67fe691fb1f9974c2b5f3753e11488
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-05-13 13:03:40 +1000

    Config module

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+from .cli_args import CLIArgs
+
+
+cli_arguments = CLIArgs()
+
+
+__all__ = ("cli_arguments",)
---
+++ config/cli_args.py
@@ -0,0 +1,37 @@
+from typing import NamedTuple
+
+
+class Argument(NamedTuple):
+    short_flag: str
+    long_flag: str
+    required: bool
+    description: str
+    aliase: str | None
+    metavar: str | None
+    default: str | int | None
+    # add an other properties if you need
+
+
+class CLIArgs(object):
+    def __init__(self) -> None:
+        self.file = Argument(
+            short_flag="-f",
+            long_flag="--file",
+            required=True,
+            description="input path/to/file.evtx",
+            aliase="input_path",
+            metavar="",
+            default=None,
+        )
+        self.output = Argument(
+            short_flag="-o",
+            long_flag="--output",
+            required=False,
+            description="output path/to/file.txt",
+            aliase="output_path",
+            metavar="",
+            default="./",
+        )
+
+    def get_arg_data(self, arg_name: str) -> NamedTuple:
+        return getattr(self, arg_name)
